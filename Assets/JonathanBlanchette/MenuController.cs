098085262fc7c58d034afe6665d583e5bf7460f6
Seryes <jonathan_b07@hotmail.fr>
2019-10-31 02:15:33 -0400

    -ajout - Package : Post processing, shader graph, riderin light weigth, cinemachine -Ajout d 'asset maya -creation de shader et des capsule de slot  pour la selection de ship
    -Creation d'un debut de script MenuController
    
    -Peace out

---
+++ Assets/JonathanBlanchette/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_SlotOne;
+    [SerializeField]
+    private GameObject m_SlotTwo;
+    [SerializeField]
+    private GameObject m_SlotThree;
+
+
+
+
+
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            
+        }
+    }
+}
