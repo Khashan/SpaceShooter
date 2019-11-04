eca23952e90cdb82f2c010c6eb0126b097726ced
jeremybeaulieupitre <jijipitre@hotmail.com>
2019-11-04 13:02:49 -0500

    Ajouts de ma scene, création de ma GameScene et de dossier, et création d'un nouveau script PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float m_Speed;
+    private float m_Tilt;
+
+    [SerializeField]
+    private Rigidbody m_Rb;
+
+    private Vector3 m_Input = new Vector3();
+
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        GetInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void GetInputs()
+    {
+        m_Input.x = Input.GetAxisRaw("Horizontal");
+        m_Input.y = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetButton("Fire1"))
+        {
+            
+        }
+    }
+
+    
+}
