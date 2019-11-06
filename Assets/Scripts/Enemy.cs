dd505ec01571bb8d8f163e59ce64eb5e6025fac7
Khashan <simonandersonl@hotmail.com>
2019-11-06 17:23:54 -0500

    Implemented Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
-{   
+public class Enemy : PooledObject, IDamageable
+{
     private float m_CurrentSpeed;
-	private float m_TargetManeuver;
+    private float m_TargetManeuver;
 
     [SerializeField]
     private float m_Speed = 5f;
@@ -18,52 +17,57 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private GameObject m_Shot;
     [SerializeField]
-	private Transform m_ShotSpawn;
+    private Transform m_ShotSpawn;
     [SerializeField]
-	private float m_FireRate = 1.5f;
+    private float m_FireRate = 1.5f;
     [SerializeField]
-	private float m_Delay = 0.5f;
+    private float m_Delay = 0.5f;
+    [SerializeField]
+    private float m_BulletSpeed = .5f;
+    [SerializeField]
+    private AudioClip m_AttackSound;
+    [SerializeField]
+    private GameObject m_ExplosionEffect;
 
     [Header("EvasiveManeuver")]
     [SerializeField]
-	private float m_Tilt = 10f;
+    private float m_Tilt = 10f;
     [SerializeField]
-	private float m_Dodge = 5f;
+    private float m_Dodge = 5f;
     [SerializeField]
-	private float m_Smoothing = 7.5f;
+    private float m_Smoothing = 7.5f;
     [SerializeField]
-	private Vector2 m_StartWait = new Vector2();
+    private Vector2 m_StartWait = new Vector2();
     [SerializeField]
-	private Vector2 m_ManeuverTime = new Vector2();
+    private Vector2 m_ManeuverTime = new Vector2();
     [SerializeField]
-	private Vector2 m_ManeuverWait = new Vector2();
-
+    private Vector2 m_ManeuverWait = new Vector2();
 
     private void Awake()
     {
         SetData();
     }
 
-	private void Start ()
-	{
+    private void Start()
+    {
         m_RB.velocity = transform.forward * m_Speed;
         m_CurrentSpeed = m_RB.velocity.z;
-		InvokeRepeating ("Fire", m_Delay, m_FireRate);
+        InvokeRepeating("Fire", m_Delay, m_FireRate);
         StartCoroutine(Evade());
-	}
+    }
 
 
-    IEnumerator Evade ()
-	{
-		yield return new WaitForSeconds (Random.Range (m_StartWait.x, m_StartWait.y));
-		while (true)
-		{
-			m_TargetManeuver = Random.Range (1, m_Dodge) * -Mathf.Sign (transform.position.x);
-			yield return new WaitForSeconds (Random.Range (m_ManeuverTime.x, m_ManeuverTime.y));
-			m_TargetManeuver = 0;
-			yield return new WaitForSeconds (Random.Range (m_ManeuverWait.x, m_ManeuverWait.y));
-		}
-	}
+    private IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(m_StartWait.x, m_StartWait.y));
+        while (true)
+        {
+            m_TargetManeuver = Random.Range(1, m_Dodge) * -Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(m_ManeuverTime.x, m_ManeuverTime.y));
+            m_TargetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(m_ManeuverWait.x, m_ManeuverWait.y));
+        }
+    }
 
     private void SetData()
     {
@@ -76,33 +80,36 @@ public class Enemy : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {        
+    {
         Evasive();
 
     }
 
-    private void OnTriggerEnter(Collider aCol)
-    {
-        if(aCol.tag == "Destroy")
-            {
-                Destroy(gameObject);
-            }
-    }
-
     private void Evasive()
     {
-            
-		m_RB.rotation = Quaternion.Euler (0, 0, m_RB.velocity.x * -m_Tilt);
-        float newManeuver = Mathf.MoveTowards (m_RB.velocity.x, m_TargetManeuver, m_Smoothing * Time.deltaTime);
-		m_RB.velocity = new Vector3 (newManeuver, 0.0f, m_CurrentSpeed);
-		
-		m_RB.rotation = Quaternion.Euler (0, 0, m_RB.velocity.x * -m_Tilt);
+
+        m_RB.rotation = Quaternion.Euler(0, 0, m_RB.velocity.x * -m_Tilt);
+        float newManeuver = Mathf.MoveTowards(m_RB.velocity.x, m_TargetManeuver, m_Smoothing * Time.deltaTime);
+        m_RB.velocity = new Vector3(newManeuver, 0.0f, m_CurrentSpeed);
+
+        m_RB.rotation = Quaternion.Euler(0, 0, m_RB.velocity.x * -m_Tilt);
     }
 
-	private void Fire ()
-	{
-		Instantiate(m_Shot, m_ShotSpawn.position, m_ShotSpawn.rotation);
-		GetComponent<AudioSource>().Play();
-	}
+    private void Fire()
+    {
+        Bullets pooledBullet = PoolManager.Instance.UseObjectFromPool<Bullets>(m_Shot, m_ShotSpawn.position, m_ShotSpawn.rotation);
+        pooledBullet.BulletInit(1, m_BulletSpeed);
 
+        AudioManager.Instance.PlaySFX(m_AttackSound, transform.position);
+    }
+
+    public void DamageReceived(int aDamageReceived)
+    {
+        PoolManager.Instance.UseObjectFromPool(m_ExplosionEffect, transform.position, transform.rotation);
+        gameObject.SetActive(false);
+    }
+
+    public void HealReceived(int aHealReceived)
+    {
+    }
 }
